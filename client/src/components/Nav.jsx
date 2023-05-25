d5aeb59f46a37d061f48d29987931982baa01e6c
brianwoo2 <brianwoo0412@gmail.com>
2023-05-25 12:17:43 -0400

    created the home page, signup and login forms (no styling)

---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+import '../style/styles.css'
+
+const Nav = ({ path }) => {
+  const navigate = useNavigate();
+  const excludedPaths = ["/login", "/sign_up"];
+
+  return (
+    <nav className="nav__container">
+      <button className="button" onClick={() => navigate('/')}>Home</button>
+      
+      {!excludedPaths.includes(path) && (
+        <>
+            <button className="button" onClick={() => navigate('/login')}>Sign in</button>
+            <button className="button__signup" onClick={() => navigate('/sign_up')}>Sign up</button>
+        </>
+      )}
+    </nav>
+  );
+};
+
+export default Nav;
--- server/app.py
+++ server/app.py
@@ -1,10 +1,16 @@
+from flask import Flask, send_from_directory
 from flask import Flask
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../client/build', static_url_path='/')
 
-@app.route('/')
-def index():
-    return 'HELLO WORLD'
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
