7687bd30ab764c7ecfffaeecff6ddc5a1172e7cf
brianwoo2 <brianwoo0412@gmail.com>
2023-05-30 12:49:09 -0400

    added recipes for users to find healthy options to eat

--- client/src/components/Recipes.jsx
+++ client/src/components/Recipes.jsx
@@ -11,7 +11,7 @@ const Recipe = () => {
         fetch(`https://webknox-recipes.p.rapidapi.com/recipes/quickAnswer?q=${encodedQuestion}`, {
             method: 'GET',
             headers: {
-                'X-RapidAPI-Key': process.env.REACT_APP_RECIPE_API_KEY,
+                'X-RapidAPI-Key': process.env.REACT_APP_QUESTION_API_KEY,
                 'X-RapidAPI-Host': 'webknox-recipes.p.rapidapi.com'
             }
         })
@@ -20,18 +20,76 @@ const Recipe = () => {
         .catch(error => console.error('Error:', error));
     }
 
+    const [query, setQuery] = useState('');
+    const [cuisine, setCuisine] = useState('');
+    const [excludeCuisine, setExcludeCuisine] = useState('');
+    const [diet, setDiet] = useState('');
+    const [intolerances, setIntolerances] = useState('');
+    const [recipes, setRecipes] = useState('');
+
+    const fetchRecipes = () => {
+        const url = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query=${query}&cuisine=${cuisine}&excludeCuisine=${excludeCuisine}&diet=${diet}&intolerances=${intolerances}`;
+        
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': process.env.REACT_APP_SPOONACULAR_API_KEY,
+                'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            const recipePromises = data.results.map(recipe =>
+                fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${recipe.id}/information`, {
+                    method: 'GET',
+                    headers: {
+                        'X-RapidAPI-Key': process.env.REACT_APP_SPOONACULAR_API_KEY,
+                        'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+                    }
+                }).then(res => res.json())
+            );
+    
+            return Promise.all(recipePromises);
+        })
+        .then(recipeDetails => {
+            setRecipes(recipeDetails);
+        })
+        .catch(error => console.error('Error:', error));
+    }
+    
+
+    console.log(recipes);
+
     return (
         <div>
             <h1>Recipe Information</h1>
+            
+            <h3>Search for recipes</h3>
+            <form onSubmit={e => { e.preventDefault(); fetchRecipes(); }}>
+                <input type="text" value={query} onChange={e => setQuery(e.target.value)} placeholder="Query (Required)" required />
+                <input type="text" value={cuisine} onChange={e => setCuisine(e.target.value)} placeholder="Cuisine (Optional)" />
+                <input type="text" value={excludeCuisine} onChange={e => setExcludeCuisine(e.target.value)} placeholder="Exclude Cuisine (Optional)" />
+                <input type="text" value={diet} onChange={e => setDiet(e.target.value)} placeholder="Diet (Optional)" />
+                <input type="text" value={intolerances} onChange={e => setIntolerances(e.target.value)} placeholder="Intolerances (Optional)" />
+                <button type="submit">Fetch Recipes</button>
+            </form>
 
-            <h3>Ask simple questions for daily needs</h3> 
+            {recipes && recipes.map(recipe => (
+                <div key={recipe.id}>
+                    <img src={recipe.image} className='recipe__food__image' />
+                    <strong>{recipe.title}</strong>
+                    <div dangerouslySetInnerHTML={{ __html: recipe.summary }} />
+                </div>
+            ))}
+
             <br />
+            <h3>Ask simple questions for daily needs</h3> 
             <p>Example: "How much vitamin c is in 2 apples?"</p>
             <form onSubmit={e => { e.preventDefault(); fetchAnswer(); }}>
                 <input type="text" value={question} onChange={e => setQuestion(e.target.value)} placeholder="Ask a question" required />
                 <button type="submit">Ask</button>
             </form>
-            {answer &&  <p> <img src={answer.image} className='food__image' />  <br /> {answer.answer} </p> }
+            {answer &&  <p className='answer'> <img src={answer.image} className='food__image' />  <br /> {answer.answer} </p> }
 
         </div>
     );
