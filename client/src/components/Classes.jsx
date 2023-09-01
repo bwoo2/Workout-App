e0fc4dfc1cb3f7612b91d1008fc870df5e29e772
brianwoo2 <bwoo2@buffalo.edu>
2023-09-01 02:08:13 -0400

    added routes to classes

--- client/src/components/Classes.jsx
+++ client/src/components/Classes.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../style/classes.css';
 
 import TrainerHead from '../images/trainers/Ropes.png';
@@ -19,46 +20,46 @@ function Classes() {
       <div className="team">
         <ul className="auto-grid" role="list">
           <li>
-            <a href="" className="profile">
+            <Link to="/classes/pilates" className="profile">
               <h2 className="profile__name">Pilates</h2>
               <p>Kristen McGee</p>
               <img alt="Pilates" src={Pilates} />
-            </a>
+            </Link>
           </li>
           <li>
-            <a href={Pilates} className="profile">
+            <Link to="/classes/yoga" className="profile">
               <h2 className="profile__name">Yoga</h2>
               <p>Tara Stiles</p>
               <img alt="Yoga" src={Yoga} />
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="" className="profile">
+            <Link to="/classes/running" className="profile">
               <h2 className="profile__name">Running</h2>
               <p>Usain Bolt</p>
               <img alt="Running" src={Running} />
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="" className="profile">
+            <Link to="/classes/HIIT" className="profile">
               <h2 className="profile__name">HIIT</h2>
               <p>Chloe Ting</p>
               <img alt="HIIT" src={HIIT} />
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="" className="profile">
+            <Link to="/classes/cycling" className="profile">
               <h2 className="profile__name">Cycling</h2>
               <p>Marianne Vos</p>
               <img alt="Cycling" src={Cycling} />
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="" className="profile">
+            <Link to="/classes/weights" className="profile">
               <h2 className="profile__name">Weight Lifting</h2>
               <p>Dwayne Johnson</p>
               <img alt="Weight Lifting" src={Lifting} />
-            </a>
+            </Link>
           </li>
         </ul>
       </div>
